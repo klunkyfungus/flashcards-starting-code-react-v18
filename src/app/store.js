e54df4e1c86701c75cb73861da96a6eb333a25f3
Dking <dking568@gmail.com>
2024-02-15 15:45:59 -0500

    Create quizzesSlice and add to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import reducers
 import topicsSliceReducer from "../features/topics/topicsSlice";
+import quizzesSliceReducer from "../features/quizzes/quizzesSlice";
 
 export default configureStore({
   reducer: {
     topics: topicsSliceReducer,
+    quizzes: quizzesSliceReducer,
   },
 });
---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const quizzesSlice = createSlice({
+    name: 'quizzes',
+    initialState: {
+        /* example state for the below property
+        {
+            '456': {
+                id: '456',
+                topicId: '123',
+                name: 'quiz name',
+                cardIds: ['1','2','3']
+            }
+        }
+        */
+        quizzes: {}
+    },
+    reducers: {
+        /* example payload:
+        {
+            id: '456',
+            name: 'quiz name',
+            topicId: '123',
+            cardIds: ['1','2','3']
+        }
+        */
+        addQuiz: (state, action) => {
+            state.quizzes[action.payload.id] = {
+                id: action.payload.id,
+                name: action.payload.name,
+                topicId: action.payload.topicId,
+                cardIds: []
+            }
+        }
+    }
+});
+
+export const allQuizzesSelector = (state) => state.quizzes.quizzes;
+export const { addQuiz } = quizzesSlice.actions;
+export default quizzesSlice.reducer;
