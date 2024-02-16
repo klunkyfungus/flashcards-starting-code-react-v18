ff2f3a28681d76a24954deb75a8b4f3f77cedb03
Dking <dking568@gmail.com>
2024-02-16 09:27:17 -0500

    Create cards slice and add to store

---
+++ src/features/cards/cardsSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cardsSlice = createSlice({
+    name: 'cards',
+    initialState: {
+        /* example of cards property:
+        {
+            '789': {
+                id: '789',
+                front: 'question text',
+                back: 'answer text'
+            }
+        }
+        */
+        cards: {}
+    },
+    reducers: {
+        /* example payload for addCard:
+        {
+            id: '789',
+            front: 'front text',
+            back: 'back text'
+        }
+        */
+        addCard: (state, action) => {
+            state.cards[action.payload.id] = action.payload;
+        }
+    }
+})
+
+export const { addCard } = cardsSlice.actions;
+export default cardsSlice.reducer;
